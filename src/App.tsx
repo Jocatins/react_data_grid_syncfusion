265007d1545a0c95fcb201e1df1d723213d2bdcf
Jocatins <jocakhamen@gmail.com>
2021-12-13 09:38:48 +0100

    filter-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   GridComponent,
   ColumnDirective,
   ColumnsDirective,
-  Page,
   Inject,
-  Edit,
-  EditSettingsModel,
-  Toolbar,
-  ToolbarItems,
+  Filter,
+  FilterSettingsModel,
 } from "@syncfusion/ej2-react-grids";
-import { MaskedTextBoxComponent } from "@syncfusion/ej2-react-inputs";
-import {
-  createOrder,
-  deleteOrder,
-  getOrders,
-  updateOrder,
-} from "./orderService";
+
 import "./App.css";
+import data from "./dataSource.json";
 
 function App() {
-  const editOptions: EditSettingsModel = {
-    allowEditing: true,
-    allowAdding: true,
-    allowDeleting: true,
-    mode: "Dialog",
+  const filterOptions: FilterSettingsModel = {
+    ignoreAccent: true,
+    type: "Menu",
+    // type: "Checkbox",
+    //type: "Excel"
   };
-  const toolbarOptions: ToolbarItems[] = ["Add", "Edit", "Delete"];
-  function editTemplate(args: any) {
-    return (
-      <MaskedTextBoxComponent
-        value={args.PhoneNumber}
-        mask="000-000-000"
-        id="PhoneNumber"
-      />
-    );
-  }
-
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    getOrders().then((data) => {
-      setData(data);
-    });
-  });
-  function dataSourceChanged(state: any) {
-    if (state.action === "add") {
-      createOrder(state.data);
-    } else if (state.action === "edit") {
-      updateOrder(state.data);
-    } else if (state.requestType === "delete") {
-      console.log(state.data);
-      deleteOrder(state.data[0].OrderID);
-    }
-  }
 
   return (
     <div className="App" style={{ margin: "10%", marginTop: "5%" }}>
       <GridComponent
         dataSource={data}
-        allowPaging={true}
-        pageSettings={{ pageSize: 5 }}
         allowFiltering={true}
-        allowGrouping={true}
-        editSettings={editOptions}
-        toolbar={toolbarOptions}
-        dataSourceChanged={dataSourceChanged}
+        filterSettings={filterOptions}
       >
         <ColumnsDirective>
           <ColumnDirective
@@ -78,6 +37,7 @@ function App() {
             field="CustomerID"
             headerText="Customer ID"
             width="150"
+            filter={{ type: "Excel" }}
           />
           <ColumnDirective
             field="OrderDate"
@@ -108,7 +68,6 @@ function App() {
             field="PhoneNumber"
             headerText="Phone Number "
             width="100"
-            editTemplate={editTemplate}
           />
           <ColumnDirective
             field="Freight"
@@ -118,7 +77,7 @@ function App() {
             editType="numericedit"
           />
         </ColumnsDirective>
-        <Inject services={[Page, Edit, Toolbar]} />
+        <Inject services={[Filter]} />
       </GridComponent>
     </div>
   );
