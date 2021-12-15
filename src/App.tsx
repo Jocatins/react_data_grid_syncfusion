c2fb635a7c594688d7227c1f6fc2c73383a6f813
Jocatins <jocakhamen@gmail.com>
2021-12-15 12:21:55 +0100

    CellSelection

--- src/App.tsx
+++ src/App.tsx
@@ -5,95 +5,30 @@ import {
   ColumnsDirective,
   Inject,
   Page,
-  Toolbar,
-  PdfExport,
-  ExcelExport,
-  Grid,
+  SelectionSettingsModel,
 } from "@syncfusion/ej2-react-grids";
 import "./App.css";
 import data from "./dataSource.json";
 
 function App() {
-  let grid: Grid | null;
-  const toolbarClick = (args: any) => {
-    if (grid) {
-      if (args.item.id.includes("pdfexport")) {
-        grid.pdfExport({
-          fileName: "titan_sync.pdf",
-          exportType: "CurrentPage",
-          theme: {
-            header: {
-              bold: true,
-              fontColor: "#00ff00",
-              fontName: "Georgia",
-              fontSize: 12,
-            },
-            record: { fontColor: "#0000ff", fontName: "Georgia", fontSize: 8 },
-          },
-          header: {
-            fromTop: 0,
-            height: 120,
-            contents: [
-              {
-                type: "Text",
-                value: "Islanders",
-                position: { x: 0, y: 50 },
-                style: { textBrushColor: "#111111", fontSize: 12 },
-              },
-            ],
-          },
-          footer: {
-            contents: [
-              {
-                type: "Text",
-                value: "Have a great day",
-                position: { x: 0, y: 50 },
-                style: { textBrushColor: "#000000", fontSize: 12 },
-              },
-            ],
-            fromBottom: 130,
-            height: 120,
-          },
-        });
-      } else if (args.item.id.includes("excelexport")) {
-        grid.excelExport({
-          fileName: "sphinx.xlsx",
-          exportType: "CurrentPage",
-          header: {
-            headerRows: 2,
-            rows: [
-              {
-                cells: [
-                  {
-                    colSpan: 4,
-                    value: "Titans Exploit",
-                    style: {
-                      fontColor: "#C67878",
-                      fontSize: 12,
-                      hAlign: "Center",
-                      bold: true,
-                    },
-                  },
-                ],
-              },
-            ],
-          },
-        });
-      }
-    }
+  const selectionOptions: SelectionSettingsModel = {
+    // type: "Multiple",
+    // mode: "Cell",
+    // cellSelectionMode: "Box",
+    checkboxMode: "ResetOnRowClick",
+    enableToggle: true,
   };
+
   return (
     <div className="App" style={{ margin: "10%", marginTop: "5%" }}>
       <GridComponent
         dataSource={data}
-        ref={(grids) => (grid = grids)}
         allowPaging={true}
-        toolbar={["PdfExport", "ExcelExport"]}
-        allowPdfExport={true}
-        allowExcelExport
-        toolbarClick={toolbarClick}
+        selectionSettings={selectionOptions}
+        selectedRowIndex={5}
       >
         <ColumnsDirective>
+          <ColumnDirective type="checkbox" width="50" />
           <ColumnDirective
             field="OrderID"
             headerText="Invoice ID"
@@ -141,11 +76,10 @@ function App() {
             headerText="Freight "
             format="C2"
             width="100"
-            editType="numericedit"
           />
         </ColumnsDirective>
 
-        <Inject services={[Page, Toolbar, PdfExport, ExcelExport]} />
+        <Inject services={[Page]} />
       </GridComponent>
     </div>
   );
